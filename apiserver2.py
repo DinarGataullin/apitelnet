f32e29d8a606a6b7fd92184f87e293200b9a475a
Dinar <gataullin-dd@yandex.ru>
2016-06-09 00:06:43 +0500

    recieve data

--- apiserver2.py
+++ apiserver2.py
@@ -7,7 +7,7 @@ import sys
 from thread import *
 
 HOST = ''  # Symbolic name meaning all available interfaces
-PORT = 8888  # Arbitrary non-privileged port
+PORT = 8000  # Arbitrary non-privileged port
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
