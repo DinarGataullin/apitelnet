8211937e1e496ba60411c34c4f37b38468ac5e7c
Dinar <gataullin-dd@yandex.ru>
2016-06-09 01:22:12 +0500

    add token checking

--- apiserver2.py
+++ apiserver2.py
@@ -1,19 +1,33 @@
 import socket
 import sys
-import sqlite3
+import logging
+import psycopg2, psycopg2.extensions
 from thread import *
+
+logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.DEBUG)
 
 HOST = ''
 PORT = 8000
 FUNCSPUB = ['getrate', 'getvendors', 'getpublicproduct', 'getsellrate']
 FUNCSPRIVATE = ['getbalance', 'getdailyusagebydestination', 'getgailyotalusage', 'getdailyusagebytrunk', 'getdailyusagebyvendor', 'gettrunklist', 'getroutingfortrunk', 'adddestinationtotrunk', 'removedestinatinofromtrunk', 'checkcdr']
+DBPARAMS = {
+    'dbname': 'icxp_v5',
+    'user': 'postgres',
+    'host': 'localhost'
+}
+
+try:
+    pgcon = psycopg2.connect(**DBPARAMS)
+    cur = pgcon.cursor(cursor_factory=psycopg2.extensions.cursor)
+except:
+    logging.error("DB connect error!")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
     s.bind((HOST, PORT))
 except socket.error as msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    logging.error('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
     sys.exit()
 
 s.listen(1000)
@@ -22,11 +36,17 @@ s.listen(1000)
 def parsetelnet(recived):
     recived = recived[:-2].split(' ')
     if recived[0].lower() in FUNCSPUB:
-        return "public: " + recived[0]
+        return "Result for public function " + recived[0]
     elif recived[0].lower() in FUNCSPRIVATE:
-        return "private: " + recived[0]
+        return "Result for private function " + recived[0]
     else:
-        return recived
+        return "Function not exist"
+
+
+def takeauthtoken(username, password):
+    query = "SELECT * FROM apiservertable WHERE username="+username
+    cur.execute(query)
+
 
 
 def clientthread(conn):
@@ -46,7 +66,7 @@ def clientthread(conn):
 
 while 1:
     conn, addr = s.accept()
-    print 'Connected client: ' + addr[0] + ':' + str(addr[1])
+    logging.info('Connected client: ' + addr[0] + ':' + str(addr[1]))
     start_new_thread(clientthread, (conn,))
 
 s.close()
